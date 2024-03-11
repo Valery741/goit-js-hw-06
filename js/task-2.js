90cdea5b87c474024eea5aaab32b13613db586ee
Valery741 <lkaraulnaya@gmail.com>
2024-03-11 20:44:31 +0100

    upd

--- js/task-2.js
+++ js/task-2.js
@@ -1,14 +1,23 @@
 class Storage {
     #items;
 
+    constructor(initialItems = []) {
+        this.#items = initialItems;
+    }
+
     getItems() {
         return this.#items;
     }
+
     addItem(newItem) {
         this.#items.push(newItem);
     }
+
     removeItem(itemToRemove) {
-        this.#items = this.#items.filter(item => item !== itemToRemove);
+        const index = this.#items.indexOf(itemToRemove);
+        if (index !== -1) {
+            this.#items.splice(index, 1);
+        }
     }
 }
 
--- js/task-3.js
+++ js/task-3.js
@@ -4,17 +4,21 @@ class StringBuilder {
     constructor(initialValue = '') {
         this.#value = initialValue;
     }
+
     getValue() {
-        return this.#value
+        return this.#value;
     }
+
     padEnd(str) {
-        this.#value.push(str);
+        this.#value += str;
     }
+
     padStart(str) {
-        this.#value = str.concat(this.#value);
+        this.#value = str + this.#value;
     }
+
     padBoth(str) {
-        this.#value = `${str}${this.#value}${str}`;
+        this.#value = str + this.#value + str;
     }
 }
 
